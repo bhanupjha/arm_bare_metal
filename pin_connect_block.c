1402919c4685f5b36491cc4b12124841ed02c42a
Bhanu Prakash <mailtobhanup@gmail.com>
2026-07-18 22:25:05 +0530

    working on pin connect block

--- pin_connect_block.c
+++ pin_connect_block.c
@@ -1,5 +1,17 @@
 #include "pin_connect_block.h"
+#include<lpc21xx.h>
 void cfgportpin(u32 portno, u32 pinno, u32 pinfun)
 {
-	
+	if(portno==0)
+	{
+		if(pinno<16)
+		{
+			PINSEL0=PINSEL0&~(3<<(pinno*2))|(pinfun<<(pinno*2));
+		}
+		else if(pinno>=16 && pinno<=31)
+		{
+			PINSEL1=PINSEL1&~(3<<((pinno-16)*2))|(pinfun<<((pinno-16)*2));
+		}
+	}
 }
+
