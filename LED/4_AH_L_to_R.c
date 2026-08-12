0bd7e1139cf59fc80c931d365052770a638d2b2d
Bhanu Prakash <mailtobhanup@gmail.com>
2026-08-12 23:47:36 +0530

    working on led

--- LED/4_AH_L_to_R.c
+++ LED/4_AH_L_to_R.c
@@ -1,22 +1,22 @@
-// 4 AH LED turn on one by one from right to  left w.r.t 500 ms delay from R to L
+// 4 AH LED turn on one by one from left to  right w.r.t 500 ms delay from L to R
 
-#define AH_LED_start_bit 0
+#define AH_LED_start_bit 3
 #include<lpc21xx.h>
 #include "delay_header.h"
 #include "typedef.h"
 int main()
 {
-	u32 i;
+	s32 i;
 	// cfg po.0 to po.3 AL_LED  as o/p
-	IODIR0 |= 15<<AL_LED_start_bit;
+	IODIR0 |= 15<<0;
 	
 	// turn off all LED
-	IOCLR0 = 15<<AL_LED_start_bit;
+	IOCLR0 = 15<<0;
 
 	while(1)
 	{
 	
-	for(i= AL_LED_start_bit; i<=3; i++)
+	for(i=3; i>=0; i--)
 	{
 		// write i -> port pin one by one logic 1-> make pin high
 		IOSET0 = 1<<i;
